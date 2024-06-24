6ab61821663c9eb3671d0c0bd5c7292f433a1b58
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-06-25 00:20:42 +0100

    register page design

--- src/Componets/Register/Register.jsx
+++ src/Componets/Register/Register.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import { FaFacebook } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
 
 const Register = () => {
-
   // form control
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +18,6 @@ const Register = () => {
           </h1>
 
           <div className="px-10 py-2 md:py-4">
-
             {/* first name */}
             <input
               type="text"
@@ -63,23 +63,38 @@ const Register = () => {
             />
 
             {/* button */}
-            <button className="btn bg-[#4ea7b3] w-full font-bold my-2">Create an account</button>
+            <button className="btn bg-[#4ea7b3] w-full font-bold my-2">
+              Create an account
+            </button>
 
             {/* Already have an account? Login */}
-            <h1 className="my-2 text-sm flex justify-center font-medium">Already have an account ?  <Link to='/login' className="font-bold ml-2 text-[#4ea7b3]">Login</Link></h1>
+            <h1 className="my-2 text-sm flex justify-center font-medium">
+              Already have an account ?{" "}
+              <Link to="/login" className="font-bold ml-2 text-[#4ea7b3]">
+                Login
+              </Link>
+            </h1>
           </div>
         </form>
       </div>
 
-         {/* or */}
-         <h1 className="my-5 font-bold text-base text-center">Or</h1>
+      <div className="flex flex-col justify-center px-5">
+        {/* or */}
+        <h1 className="my-5 font-bold text-base text-center">Or</h1>
 
-         <div>
-              <div>
-                  <h1>Continue with Facebook</h1>
-              </div>
-         </div>
-      
+        {/* connect facebook */}
+          <div className="flex gap-5 border rounded-2xl px-2 py-2 btn bg-white hover:bg-white my-1">
+              <FaFacebook className="bg-[white] text-[#3076FF] text-[24px]"></FaFacebook>
+              <h1 className="font-medium text-[16px]">Continue with Facebook</h1>
+          </div>
+
+         {/* connect google */}
+         <div className="flex gap-5 border rounded-2xl px-2 py-2 btn bg-white hover:bg-white my-1">
+              < FcGoogle className="bg-[white] text-[#3076FF] text-[24px]"></ FcGoogle>
+              <h1 className="font-medium text-[16px]">Continue with Google</h1>
+          </div>  
+    
+      </div>
     </div>
   );
 };
