33756e5163e3bfdfdcb1f8e47299edbe175a3e9d
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-06-27 01:08:34 +0600

    date retreive succesfully

--- src/Componets/Dashboard/Components/UsersCollection/SingleUser.jsx
+++ src/Componets/Dashboard/Components/UsersCollection/SingleUser.jsx
@@ -1,18 +1,46 @@
+import { MdAutoDelete } from "react-icons/md";
+
+const SingleUser = ({ user, index }) => {
+  const { displayName, email, role } = user;
+
+//   const handleDelete = (email) => {
+//     deleteUserData(email);
+//     toast.success("User delete successfully");
+//   };
+
+  return (
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th> {index + 1}</th>
 
 
-const SingleUser = ({user}) => {
-    return (
-        <div>
-        {/* row 1 */}
-        <tr>
-           <th>1</th>
-           <td>Cy Ganderton</td>
-           <td>Quality Control Specialist</td>
-           <td>Blue</td>
-         </tr>
-   </div>
-);
-    
+        <td>
+          <div>
+            <div className="font-bold">{displayName}</div>
+          </div>
+        </td>
+
+        <td>
+          <div>
+            <div className="font-bold">{email}</div>
+          </div>
+        </td>
+        <th>
+          <button className="btn btn-ghost btn-xs">{role}</button>
+        </th>
+
+        <th>
+          <button
+            // onClick={() => handleDelete(singleUser.email)}
+            className="btn btn-secondary text-base "
+          >
+            <MdAutoDelete></MdAutoDelete>
+          </button>
+        </th>
+      </tr>
+    </tbody>
+  );
 };
 
 export default SingleUser;
--- src/Componets/Dashboard/Components/UsersCollection/Users.jsx
+++ src/Componets/Dashboard/Components/UsersCollection/Users.jsx
@@ -1,37 +1,43 @@
 import { useLoaderData } from "react-router-dom";
+import SingleUser from "./SingleUser";
 
 
 
 const Users = () => {
   const usersCollection = useLoaderData();
+  const users = usersCollection.filter(user => user.role == 'user')
 
   return (
     <div className="py-5">
       <h1 className="text-2xl font-bold animate-pulse text-[green]">
-        All Users : <span className="text-[red]">{usersCollection.length}</span>
+        All Users : <span className="text-[red]">{users.length}</span>
       </h1>
 
-      <div className="px-2">
-        <div className="overflow-x-auto border">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-              </tr>
-            </thead>
-            <tbody>
-                   {
-                       usersCollection.map(user=>)
-                   }
-             
-            </tbody>
-          </table>
-        </div>
-      </div>
+       {/*  user data show in table format */}
+
+       <div className="overflow-x-auto mt-16">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Delete</th>
+                            
+                        </tr>
+                    </thead>
+
+                    {
+                        users.map((user, index) => <SingleUser index={index} key={user._id} user={user}></SingleUser>)
+                    }
+
+
+
+
+                </table>
+            </div>
     </div>
   );
 };
