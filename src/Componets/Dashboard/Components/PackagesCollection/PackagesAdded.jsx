7744af543ef86e4148048b731bdd92d42e6053e2
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-06-29 08:23:43 +0600

    packages input value get

--- src/Componets/Dashboard/Components/PackagesCollection/PackagesAdded.jsx
+++ src/Componets/Dashboard/Components/PackagesCollection/PackagesAdded.jsx
@@ -1,28 +1,60 @@
-
+import { UploadImage } from "../Hook/UploadImage";
 
 const PackagesAdded = () => {
+  const handlePackages = async (e) => {
+    e.preventDefault();
 
-    const handleHotels=()=>{
+    const form = e.target;
 
-    }
-
-
-    return (
-        <div>
-        <h1 className="text-2xl text-center text-[green] font-bold py-5">
-          New Packages Added
-        </h1>
-  
-        {/*  new packages added */}
-        <form
-          onSubmit={handleHotels}
-          className="border-4 rounded xl:mx-2 bg-slate-400 my-12 flex flex-col justify-center py-5 mx-2"
-        >
+    const placeName = form.placeName.value;
+    const Image1 = form.placeImage.files[0];
+    const placeLocation = form.placeLocation.value;
+    const placeDescription = form.placeDescription.value;
+    const hotelName = form.hotelName.value;
+    const Image2 = form.hotelImage.files[0];
+    const hotelLocation = form.hotelLocation.value;
+    const hotelDescription = form.hotelDescription.value;
+    const postedDate = form.postedDate.value;
 
 
+      // upload image
+      const imageData1 = await UploadImage(Image1);
+      const imageData2 = await UploadImage(Image2);
 
-          {/* travel place name  */}
-          <div className="m-5 text-left">
+      const placeImage = imageData1?.data?.url;
+      const hotelImage = imageData2?.data?.url;
+
+
+      const newPackages = {
+        placeName:placeName,
+        placeImage:placeImage,
+        placeLocation:placeLocation,
+        placeDescription:placeDescription,
+        hotelName:hotelName,
+        hotelImage:hotelImage,
+        hotelLocation:hotelLocation,
+        hotelDescription:hotelDescription,
+        postedDate:postedDate
+
+        };
+
+        console.log(newPackages)
+
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl text-center text-[green] font-bold py-5">
+        New Packages Added
+      </h1>
+
+      {/*  new packages added */}
+      <form
+        onSubmit={handlePackages}
+        className="border-4 rounded xl:mx-2 bg-slate-400 my-12 flex flex-col justify-center py-5 mx-2"
+      >
+        {/* travel place name  */}
+        <div className="m-5 text-left">
           <h1 className="text-lg md:text-xl text-left font-bold py-2">
             Travel place name{" "}
           </h1>
@@ -83,97 +115,97 @@ const PackagesAdded = () => {
           ></textarea>
         </div>
 
-          {/* travel hotel name  */}
-          <div className="m-5 text-left">
+        {/* travel hotel name  */}
+        <div className="m-5 text-left">
+          <h1 className="text-lg md:text-xl text-left font-bold py-2">
+            Travel hotel name{" "}
+          </h1>
+          <input
+            type="text"
+            name="hotelName"
+            id=""
+            placeholder="Enter travel hotel name"
+            className="font-bold w-full my-2 p-2 rounded"
+            required
+          />
+        </div>
+
+        {/* travel hotel image  */}
+        <div className="m-5 text-left grid md:grid-cols-3 gap-2 ">
+          <div className="">
             <h1 className="text-lg md:text-xl text-left font-bold py-2">
-              Travel hotel name{" "}
+              Travel hotel image{" "}
+            </h1>
+            <input
+              type="file"
+              name="hotelImage"
+              id=""
+              className="w-full my-2 py-2 rounded"
+              required
+            />
+          </div>
+
+          {/* hotel location  */}
+          <div className=" col-span-2">
+            <h1 className="text-lg md:text-xl text-left font-bold py-2">
+              Travel hotel location
             </h1>
             <input
               type="text"
-              name="hotelName"
+              name="hotelLocation"
               id=""
-              placeholder="Enter travel hotel name"
+              placeholder="Enter travel hotel location"
               className="font-bold w-full my-2 p-2 rounded"
               required
             />
           </div>
-  
-          {/* travel hotel image  */}
-          <div className="m-5 text-left grid md:grid-cols-3 gap-2 ">
-            <div className="">
-              <h1 className="text-lg md:text-xl text-left font-bold py-2">
-                Travel hotel image{" "}
-              </h1>
-              <input
-                type="file"
-                name="hotelImage"
-                id=""
-                className="w-full my-2 py-2 rounded"
-                required
-              />
-            </div>
-  
-            {/* hotel location  */}
-            <div className=" col-span-2">
-              <h1 className="text-lg md:text-xl text-left font-bold py-2">
-                Travel hotel location
-              </h1>
-              <input
-                type="text"
-                name="hotelLocation"
-                id=""
-                placeholder="Enter travel hotel location"
-                className="font-bold w-full my-2 p-2 rounded"
-                required
-              />
-            </div>
-          </div>
-  
-          {/* travel  hotel description  */}
-          <div className="m-5 text-left">
+        </div>
+
+        {/* travel  hotel description  */}
+        <div className="m-5 text-left">
+          <h1 className="text-lg md:text-xl text-left font-bold py-2">
+            Travel hotel Description{" "}
+          </h1>
+
+          <textarea
+            rows={5}
+            name="hotelDescription"
+            id=""
+            placeholder="Write travel hotel description ........."
+            className="font-bold w-full my-2 p-2 rounded"
+            required
+          ></textarea>
+        </div>
+
+        {/* submit button  */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 m-5">
+          {/* posted date */}
+          <div className=" ">
             <h1 className="text-lg md:text-xl text-left font-bold py-2">
-              Travel hotel Description{" "}
+              Posted Date
             </h1>
-  
-            <textarea
-              rows={5}
-              name="hotelDescription"
+            <input
+              type="date"
+              name="postedDate"
               id=""
-              placeholder="Write travel hotel description ........."
+              placeholder=""
               className="font-bold w-full my-2 p-2 rounded"
               required
-            ></textarea>
+            />
           </div>
-  
-          {/* submit button  */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 m-5">
-            {/* posted date */}
-            <div className=" ">
-              <h1 className="text-lg md:text-xl text-left font-bold py-2">
-                Posted Date
-              </h1>
-              <input
-                type="date"
-                name="postedDate"
-                id=""
-                placeholder=""
-                className="font-bold w-full my-2 p-2 rounded"
-                required
-              />
-            </div>
-  
-            <div className=" flex items-end justify-center md:justify-end">
-              <button
-                type="submit"
-                className="btn w-1/2 my-2 p-2 rounded font-extrabold text-base"
-              >
-                Post
-              </button>
-            </div>
+
+          <div className=" flex items-end justify-center md:justify-end">
+            <button
+              type="submit"
+              className="btn w-1/2 my-2 p-2 rounded font-extrabold text-base"
+            >
+              Post
+            </button>
           </div>
-        </form>
-      </div>
-    );
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default PackagesAdded;
