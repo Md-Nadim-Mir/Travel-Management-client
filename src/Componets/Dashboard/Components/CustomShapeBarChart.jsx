3af45e31022960bdda39cd84b7d9c8371e39bf79
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-08-23 02:03:09 +0600

    pie chart added

--- src/Componets/Dashboard/Components/CustomShapeBarChart.jsx
+++ src/Componets/Dashboard/Components/CustomShapeBarChart.jsx
@@ -5,6 +5,11 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
 
+
+
+const CustomShapeBarChart = () => {
+
+
 // data load 
 
 // users length find
@@ -16,72 +21,122 @@ fetch('http://localhost:3000/users')
 
 // users lenth end 
 
-const data = [
-  {
-    name: 'Users',
-    uv: user.length,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 10,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 6,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 9,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 7,
-    pv: 3800,
-    amt: 2500,
-  },
+
+ // places length find
+ const [place, setPlace] = useState([]);
+
+  
+ fetch('http://localhost:3000/places')
+   .then((res) => res.json())
+   .then((places) => setPlace(places));
+
+ // places lenth end 
+
+
+ // hotels length find
+ const [hotel, setHotel] = useState([]);
+
  
-];
+ fetch('http://localhost:3000/hotels')
+   .then((res) => res.json())
+   .then((hotels) => setHotel(hotels));
 
-const getPath = (x, y, width, height) => {
-    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
-    ${x + width / 2}, ${y}
-    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-    Z`;
-  };
+ // hotels lenth end 
+
+
+ // packages length find
+ const [packaged, setPackage] = useState([]);
+
+ 
+ fetch('http://localhost:3000/packages')
+   .then((res) => res.json())
+   .then((packages) => setPackage(packages));
+
+ // packages lenth end 
+
+
+ // blogs length find
+ const [blog, setBlog] = useState([]);
+
+ 
+ fetch('http://localhost:3000/blogs')
+   .then((res) => res.json())
+   .then((blogs) => setBlog(blogs));
+
+ // blogs lenth end 
+
+
+ // booking length find
+ const [booking, setBooking] = useState([]);
+
+ 
+ fetch('http://localhost:3000/blogs')
+   .then((res) => res.json())
+   .then((bookings) => setBooking(bookings));
+
+ // bookings lenth end 
+
+const data = [
+    {
+      name: 'Users',
+      uv: user.length,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Places',
+      uv: place.length,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Hotels',
+      uv: hotel.length,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Packages',
+      uv: packaged.length,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Blogs',
+      uv: blog.length,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Bookings',
+      uv: booking.length,
+      pv: 3800,
+      amt: 2500,
+    },
+   
+  ];
   
-  const TriangleBar = (props) => {
-    const { fill, x, y, width, height } = props;
-  
-    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-  };
-
-const CustomShapeBarChart = () => {
-
+  const getPath = (x, y, width, height) => {
+      return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+      ${x + width / 2}, ${y}
+      C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+      Z`;
+    };
+    
+    const TriangleBar = (props) => {
+      const { fill, x, y, width, height } = props;
+    
+      return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
 
     return (
         <BarChart
-        width={500}
+        width={700}
         height={300}
         data={data}
         margin={{
           top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5,
+          bottom: 10,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
--- src/Componets/Dashboard/Components/Statistics.jsx
+++ src/Componets/Dashboard/Components/Statistics.jsx
@@ -1,72 +1,61 @@
 import { useState } from "react";
 import CustomShapeBarChart from "./CustomShapeBarChart";
+import PieChartWithCustomizedLabel from "./PieChartWithCustomizedLabel";
 
 const Statistics = () => {
-  
-  
   // users length find
   const [user, setUser] = useState([]);
- 
-  fetch('http://localhost:3000/users')
+
+  fetch("http://localhost:3000/users")
     .then((res) => res.json())
     .then((users) => setUser(users));
 
-  // users lenth end 
-
+  // users lenth end
 
   // places length find
   const [place, setPlace] = useState([]);
 
-  
-  fetch('http://localhost:3000/places')
+  fetch("http://localhost:3000/places")
     .then((res) => res.json())
     .then((places) => setPlace(places));
 
-  // places lenth end 
-
+  // places lenth end
 
   // hotels length find
   const [hotel, setHotel] = useState([]);
 
-  
-  fetch('http://localhost:3000/hotels')
+  fetch("http://localhost:3000/hotels")
     .then((res) => res.json())
     .then((hotels) => setHotel(hotels));
 
-  // hotels lenth end 
-
+  // hotels lenth end
 
   // packages length find
   const [packaged, setPackage] = useState([]);
 
-  
-  fetch('http://localhost:3000/packages')
+  fetch("http://localhost:3000/packages")
     .then((res) => res.json())
     .then((packages) => setPackage(packages));
 
-  // packages lenth end 
-
+  // packages lenth end
 
   // blogs length find
   const [blog, setBlog] = useState([]);
 
-  
-  fetch('http://localhost:3000/blogs')
+  fetch("http://localhost:3000/blogs")
     .then((res) => res.json())
     .then((blogs) => setBlog(blogs));
 
-  // blogs lenth end 
-
+  // blogs lenth end
 
   // booking length find
   const [booking, setBooking] = useState([]);
 
-  
-  fetch('http://localhost:3000/blogs')
+  fetch("http://localhost:3000/blogs")
     .then((res) => res.json())
     .then((bookings) => setBooking(bookings));
 
-  // bookings lenth end 
+  // bookings lenth end
 
   return (
     <div>
@@ -74,71 +63,67 @@ const Statistics = () => {
         Statistics Reports
       </h1>
 
-
       {/* custom statistics */}
 
       <div className="border-4 border-[#FF00D3] mx-5 rounded-lg shadow-2xl my-16">
-
         <div className="grid grid-cols-3 border-b border-[#0fd420] mx-2">
-
           {/* users */}
           <div className="border-r-2 border-red-400  my-5 ">
-            <div className="text-md md:text-xl  font-bold">
-              Total Users
-            </div>
-            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce ">{user.length}</h1>
+            <div className="text-md md:text-xl  font-bold">Total Users</div>
+            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce ">
+              {user.length}
+            </h1>
           </div>
 
           {/* places */}
           <div className="border-r-2 border-red-400  my-5 ">
-            <div className="text-md md:text-xl  font-bold">
-              Total Places
-            </div>
-            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">{place.length}</h1>
+            <div className="text-md md:text-xl  font-bold">Total Places</div>
+            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">
+              {place.length}
+            </h1>
           </div>
 
           {/* Hotels */}
           <div className="my-5 ">
-            <div className="text-md md:text-xl  font-bold">
-              Total Hotels
-            </div>
-            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">{hotel.length}</h1>
+            <div className="text-md md:text-xl  font-bold">Total Hotels</div>
+            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">
+              {hotel.length}
+            </h1>
           </div>
-
         </div>
 
         <div className="grid grid-cols-3 mx-2">
-
           {/* Packages */}
           <div className="border-r-2 border-red-400  my-5 ">
-            <div className="text-md md:text-xl  font-bold">
-              Total Packages
-            </div>
-            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">{packaged.length}</h1>
+            <div className="text-md md:text-xl  font-bold">Total Packages</div>
+            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">
+              {packaged.length}
+            </h1>
           </div>
 
           {/* Blogs */}
           <div className="border-r-2 border-red-400 my-5 ">
-            <div className="text-md md:text-xl  font-bold">
-              Total Blogs
-            </div>
-            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">{blog.length}</h1>
+            <div className="text-md md:text-xl  font-bold">Total Blogs</div>
+            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">
+              {blog.length}
+            </h1>
           </div>
 
           {/* Bookings */}
           <div className="my-5 ">
-            <div className="text-md md:text-xl  font-bold">
-              Total Bookings
-            </div>
-            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">{booking.length}</h1>
+            <div className="text-md md:text-xl  font-bold">Total Bookings</div>
+            <h1 className="text-[blue] text-3xl font-extrabold mt-2 animate-bounce">
+              {booking.length}
+            </h1>
           </div>
-
         </div>
-       
       </div>
 
       {/* chart */}
-      <CustomShapeBarChart></CustomShapeBarChart>
+    <div className="grid grid-cols-1 md:grid-cols-2 justify-between">
+        <CustomShapeBarChart></CustomShapeBarChart>
+        <PieChartWithCustomizedLabel></PieChartWithCustomizedLabel>
+      </div>
     </div>
   );
 };
