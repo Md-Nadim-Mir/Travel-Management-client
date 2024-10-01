5b109f8a501a4dff0c65f8d58a5f69121794e578
Md-Nadim-Mir <mdnadimmir150@gmail.com>
2024-10-01 15:56:51 +0600

    feedback part complete

--- src/Componets/Review/Feedback.jsx
+++ src/Componets/Review/Feedback.jsx
@@ -7,7 +7,7 @@ const Feedback = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 2000,
     slidesToShow: 3, // Number of cards to show at a time
     slidesToScroll: 1, // Number of cards to scroll at a time
     autoplay: true, // Enable auto sliding
@@ -35,14 +35,7 @@ const Feedback = () => {
     ],
   };
 
-  //   const cards = [
-  //     { id: 1, content: "Card 1" },
-  //     { id: 2, content: "Card 2" },
-  //     { id: 3, content: "Card 3" },
-  //     { id: 4, content: "Card 4" },
-  //     { id: 5, content: "Card 5" },
-  //     { id: 6, content: "Card 6" },
-  //   ];
+  
 
   let [testmonial, setTestomonial] = useState([]);
 
@@ -53,13 +46,13 @@ const Feedback = () => {
   }, []);
 
   return (
-    <div className="w-full mx-auto my-8">
+    <div className="w-full px-5 md:px-4 my-8 ">
       <h1 className="text-xl md:text-2xl font-bold mt-14 mb-8 text-[#FFA500]">
-        Cutomer Review
+        Traveller Review
       </h1>
       <Slider {...settings}>
         {testmonial.map((card) => (
-          <div key={card.id} className="px-2 h-full">
+          <div key={card.id} className=" h-full px-2">
             <div className="bg-[#ffffff] p-6 text-center border border-gray-200 rounded-lg shadow-lg">
               <div>
                 <div className="flex justify-center">
@@ -69,7 +62,7 @@ const Feedback = () => {
                     alt=""
                   />
                 </div>
-                <h1 className="text-2xl font-bold text-left">{card.name}</h1>
+                <h1 className="text-2xl font-bold text-left mt-5 mb-2">{card.name}</h1>
                 <h1 className="text-left">{card.details}</h1>
               </div>
             </div>
